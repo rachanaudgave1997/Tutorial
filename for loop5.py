39dc2ceb521fe91121a0573e3101389178fb3bc5
rachanaudgave1997 <144700265+rachanaudgave1997@users.noreply.github.com>
2023-09-11 20:12:32 +0530

    Add files via upload

---
+++ for loop5.py
@@ -0,0 +1,10 @@
+#cubes of odd number :
+
+names = ['joe', 'adam', 'john', 'don', 'smith']
+ages = [12,45,25,66,50]
+
+for i in range(len(names)):
+     print("My name is " + names[i] + ", my age is " + str(ages[i]))
+
+
+#Output:[9261, 12167, 15625, 19683, 24389, 29791, 35937, 42875, 50653, 59319]
