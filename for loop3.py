4e4f7ca5ab618435a4db7bcb7791472501a429af
rachanaudgave1997 <144700265+rachanaudgave1997@users.noreply.github.com>
2023-09-11 19:55:18 +0530

    Add files via upload

---
+++ for loop3.py
@@ -0,0 +1,8 @@
+#You can create a list of numbers from 20 to 1 :
+
+numbers = []
+for i in range(20, 0, -1):
+    numbers.append(i)
+print(numbers)
+
+#Output:[20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
