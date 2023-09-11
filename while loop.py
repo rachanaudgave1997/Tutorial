ea5ee229c08e61bfb3d073cc9d78c127a2debb19
rachanaudgave1997 <144700265+rachanaudgave1997@users.noreply.github.com>
2023-09-11 21:32:15 +0530

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,11 @@
+
+#create list 20-1 (reverse order) using while loop
+numbers=[]
+i = 20
+while i >= 1:
+    numbers.append(i)
+    i -= 1
+print(numbers)
+
+
+#Output:[20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
