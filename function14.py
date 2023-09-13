2ce041f82a5e121f24238f8fb3501b6a8079b5f8
rachanaudgave1997 <144700265+rachanaudgave1997@users.noreply.github.com>
2023-09-13 19:20:21 +0530

    Add files via upload

---
+++ function14.py
@@ -0,0 +1,12 @@
+#Define a function that accepts lowercase words and returns uppercase words.
+
+def convert_to_uppercase(word):
+    uppercase_word = word.upper()
+    return uppercase_word
+
+result = convert_to_uppercase("statistics")
+print("Result is =", result)
+
+
+
+# Output:Result is = STATISTICS
